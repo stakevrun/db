ac0c821ec74949f747564e86829dcceaef60538d
Ramana Kumar <ramana@member.fsf.org>
2024-05-16 21:04:29 +0100

    Format errors (and AggregateError) better in sck

--- sck.js
+++ sck.js
@@ -2,5 +2,11 @@ import { createConnection } from 'node:net'
 const prvPort = 6000
 const socket = createConnection(prvPort, process.env.PRV_HOST).setEncoding('utf8')
 socket.pipe(process.stdout)
-socket.on('error', error => process.stderr.write(error))
+socket.on('error', error =>
+  process.stderr.write(
+    'errors' in error ?
+      error.errors.map(e => e.toString()).join('\n')
+    : error.toString()
+  )
+)
 process.stdin.pipe(socket)
