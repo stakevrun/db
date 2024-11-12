820357fb4538113c76d1c212dddb2e76eb47b2d6
owlot <95369289+owlot@users.noreply.github.com>
2024-11-12 17:16:52 +0100

    feat: refactor prv to be a standalone tcp service

--- prv.js
+++ prv.js
@@ -5,7 +5,7 @@ import { randomSeed, privkeyFromPath, pubkeyFromPrivkey, generateKeystore, toHex
 import { bls12_381 } from '@noble/curves/bls12-381'
 import { ensureDirs, gitCheck, gitPush, workDir, chainIds, addressRegExp } from './lib.js'
 import { mkdirSync, existsSync, writeFileSync, readFileSync } from 'node:fs'
-import { createInterface } from 'node:readline'
+import { createServer } from 'node:net'
 
 ensureDirs()
 
@@ -28,75 +28,116 @@ ensureDirs()
 // keystore: return a JSON-encoded keystore for ADDRESS's key at KEYPATH (protected by KEYPASS)
 // sign:     return signature (hexstring) for MESSAGE using ADDRESS's key at KEYPATH
 
-const inputLines = []
-for await (const line of createInterface({input: process.stdin}))
-  inputLines.push(line)
+const [HOST, PORT] = process.env.PRV_HOST.split(':')
 
-const readLine = (variable) => {
-  const prefix = `${variable} = `
-  const line = inputLines.shift()
-  if (!(line && line.startsWith(prefix)))
-    throw new Error(`missing ${variable}`)
-  return line.slice(prefix.length)
-}
+createServer((stream) => {
+  console.log("Got new connection")
 
-const chainId = readLine('CHAINID')
-if (!(chainId in chainIds))
-  throw new Error('invalid chainId')
+  const inputLines = []
+  let buffer = ''
 
-const address = readLine('ADDRESS')
-if (!addressRegExp.test(address))
-  throw new Error('invalid address')
+  stream.on('data', function (data) {
+    buffer += data.toString();
+  })
 
-const chainPath = `${workDir}/${chainId}`
-const seedPath = `${chainPath}/${address}`
+  stream.on('end', function () {
+    buffer.split('\n').forEach(line => {
+      const trimmedLine = line.trim();
+      if (trimmedLine) inputLines.push(trimmedLine); // Ignore empty lines
+    });
+    handleRequest()
+    stream.end()
+  })
 
-const command = readLine('COMMAND')
+  stream.on('error', function (e) {
+    console.log(`Got error ${e}`)
+    stream.write(e)
+    console.log("Closing connection")
+    stream.end()
+  })
 
-if (command == 'generate') {
-  if (existsSync(seedPath))
-    console.log('exists')
-  else {
-    mkdirSync(chainPath, {recursive: true})
-    writeFileSync(seedPath, randomSeed(), {flag: 'wx'})
-    gitCheck(['add', seedPath], workDir, '', 'failed to add seed')
-    gitCheck(['diff', '--staged', '--name-status'], workDir,
-      output => (
-        !output.trimEnd().includes('\n') &&
-        output.trimEnd().split(/\s+/).join() == `A,${chainId}/${address}`
-      ),
-      'unexpected diff adding seed'
-    )
-    gitPush(address, workDir)
-    console.log('created')
+  stream.on('close', function (had_error) {
+    if(had_error) {
+      console.log("Connection closed unexpectedly.")
+    }
+    stream.end()
+  })
+
+  function readLine(variable) {
+    if (inputLines.length < 1) {
+      throw new Error(`readLine called for ${variable}, but no more lines to read!`)
+    }
+    const prefix = `${variable} = `
+    const line = inputLines.shift()
+    if (!(line && line.startsWith(prefix))) {
+      throw new Error(`missing ${variable}`)
+    }
+    return line.slice(prefix.length)
   }
-}
-else {
-  const seed = readFileSync(seedPath)
-  const path = readLine('KEYPATH')
-  const sk = privkeyFromPath(seed, path)
-  switch (command) {
-    case 'pubkey': {
-      const pk = pubkeyFromPrivkey(sk)
-      console.log(`0x${toHex(pk)}`)
-      break
+
+  function handleRequest() {
+    const chainId = readLine('CHAINID')
+    if (!(chainId in chainIds)) {
+      console.log(`Could not find chainId ${chainId} in chainIds list:`)
+      console.log(chainIds)
+      throw new Error('invalid chainId')
     }
-    case 'keystore': {
-      const pubkey = pubkeyFromPrivkey(sk)
-      const password = readLine('KEYPASS')
-      console.log(JSON.stringify(generateKeystore({sk, path, pubkey, password})))
-      break
+
+    const address = readLine('ADDRESS')
+    if (!addressRegExp.test(address))
+      throw new Error('invalid address')
+
+    const chainPath = `${workDir}/${chainId}`
+    const seedPath = `${chainPath}/${address}`
+
+    const command = readLine('COMMAND')
+
+    if (command == 'generate') {
+      if (existsSync(seedPath))
+        console.log('exists')
+      else {
+        mkdirSync(chainPath, {recursive: true})
+        writeFileSync(seedPath, randomSeed(), {flag: 'wx'})
+        gitCheck(['add', seedPath], workDir, '', 'failed to add seed')
+        gitCheck(['diff', '--staged', '--name-status'], workDir,
+          output => (
+            !output.trimEnd().includes('\n') &&
+            output.trimEnd().split(/\s+/).join() == `A,${chainId}/${address}`
+          ),
+          'unexpected diff adding seed'
+        )
+        gitPush(address, workDir)
+        console.log('created')
+      }
     }
-    case 'sign': {
-      const messageHex = readLine('MESSAGE')
-      if (!messageHex.startsWith('0x')) throw new Error('invalid message')
-      const message = Buffer.from(messageHex.slice(2), 'hex')
-      const htfEthereum = {DST: 'BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_'}
-      const sig = bls12_381.sign(message, sk, htfEthereum)
-      console.log(`0x${toHex(sig)}`)
-      break
+    else {
+      const seed = readFileSync(seedPath)
+      const path = readLine('KEYPATH')
+      const sk = privkeyFromPath(seed, path)
+      switch (command) {
+        case 'pubkey': {
+          const pk = pubkeyFromPrivkey(sk)
+          console.log(`0x${toHex(pk)}`)
+          break
+        }
+        case 'keystore': {
+          const pubkey = pubkeyFromPrivkey(sk)
+          const password = readLine('KEYPASS')
+          console.log(JSON.stringify(generateKeystore({sk, path, pubkey, password})))
+          break
+        }
+        case 'sign': {
+          const messageHex = readLine('MESSAGE')
+          if (!messageHex.startsWith('0x')) throw new Error('invalid message')
+          const message = Buffer.from(messageHex.slice(2), 'hex')
+          const htfEthereum = {DST: 'BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_'}
+          const sig = bls12_381.sign(message, sk, htfEthereum)
+          console.log(`0x${toHex(sig)}`)
+          break
+        }
+        default:
+          throw new Error('invalid command')
+      }
     }
-    default:
-      throw new Error('invalid command')
   }
-}
+}).listen(PORT);
